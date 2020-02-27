3a393bcc2cbb256c49703e0a9d173d1dd0932e45
Suhun Han <ssut@ssut.me>
2020-02-27 14:01:33 +0900

    전북

---
+++ app/scrappers/jeonbuk.ts
@@ -0,0 +1,40 @@
+import { ICoronaStats } from './interface';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import { DateTime } from 'luxon';
+
+export default async function () {
+  const resp = await axios.get('http://www.jeonbuk.go.kr/index.jeonbuk');
+  const $ = cheerio.load(resp.data);
+
+  const updatedAt = $('body > div > header > div.top_info2 > div > div > p').text().trim().replace(/[ \r\n\t]{1,}/g, ' ');
+  const stats = $('body > div > header > div.top_info2 > div > div > ul > li:nth-child(1)').text().replace(/[ \r\n\t]{1,}/g, ' ');
+
+  const patterns = {
+    확진자: /확진자 (?<확진자>[0-9]+)명/g,
+    입원환자: /격리 (?<입원환자>[0-9]+)명/g,
+    퇴원자: /완치 (?<퇴원자>[0-9]+)명/g,
+    사망자: /사망자 (?<사망자>[0-9]+)명/g,
+    자가격리: /자가격리 (?<자가격리>[0-9]+)명/g,
+  };
+
+  const info = {
+    updatedAt: DateTime.fromFormat(updatedAt, 'yyyy. M. d. hh:mm'),
+  } as any;
+
+  for (const [key, pattern] of Object.entries(patterns)) {
+    const result = pattern.exec(stats);
+    if (result === null) {
+      info[key] = 0;
+      continue;
+    }
+
+    info[key] = Number(result.groups?.[key].trim()) || 0;
+  }
+
+  return info;
+}
+
+if (require.main === module) {
+  module.exports.default().then(console.info);
+}
