ba8e9bde57d5c09f7da87ca251688571680e0ca1
Suhun Han <ssut@ssut.me>
2020-02-28 15:02:28 +0900

    add eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,5 +14,19 @@ module.exports = {
         NODE_ENV: 'production',
       },
     },
+    {
+      name: 'coronameter-task-runner',
+      script: 'dist/task-runner.js',
+      instances: 2,
+      autorestart: true,
+      max_memory_restart: '500M',
+      watch: false,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
   ],
 };
