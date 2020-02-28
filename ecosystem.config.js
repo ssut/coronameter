933e185e9522632a592e1675694a927a653d7258
Suhun Han <ssut@ssut.me>
2020-02-28 14:41:48 +0900

    feat: add pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'coronameter',
+      script: 'dist/main',
+      instances: 3,
+      autorestart: true,
+      max_memory_restart: '500M',
+      watch: false,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
